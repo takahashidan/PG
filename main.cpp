75347393e101068d028421abcb6854c7e59c12e2
takahashidan <k023g1019@g.neec.ac.jp>
2024-06-21 12:41:36 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿// 説明スライドを参考に、現実世界のポリフォーズムを名前にしてください
+
+#include <stdio.h>
+
+class parent {
+public:
+	virtual void Voice() { printf("あにまる¥n"); };
+};
+
+class childA : public parent {
+public:
+	void Voice() { printf("子供Aっす\n"); };
+};
+
+class childB : public parent {
+public:
+	void Voice() { printf("子供Bっす\n"); };
+};
+
+int main(void) {
+
+	parent* animal[2] = {new childA, new childB};
+
+	animal[0]->Voice();
+	animal[1]->Voice();
+
+	delete animal[0];
+	delete animal[1];
+
+	return 0;
+}
